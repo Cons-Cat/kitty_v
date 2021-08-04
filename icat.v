2c854f7fe7a015f5476cb21c4878093a02c4eb39
Conscat <wgooch2000@gmail.com>
2021-08-04 14:29:31 -0400

    Made printing take bytes instead of strings

--- icat.v
+++ icat.v
@@ -4,85 +4,6 @@ import kitty
 import flag
 import os
 
-const (
-	ppm_width  = 4
-	ppm_height = 4
-	ppm_data   = [
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(100), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(255), 0, 255]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 255, 175]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 15, 175]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(255), 0, 255]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(0), 0, 0]!
-		},
-		Pixel{
-			color: [byte(255), 255, 255]!
-		},
-	]
-)
-
-struct Pixel {
-	color [3]byte
-}
-
-fn (p Pixel) to_string() string {
-	mut str := ''
-	str += rune(p.color[0]).str()
-	str += rune(p.color[1]).str()
-	str += rune(p.color[2]).str()
-	return str
-}
-
-struct PPM {
-	data []Pixel
-}
-
-fn (p PPM) to_string() string {
-	mut str := ''
-	for pixel in p.data {
-		str += pixel.to_string()
-	}
-	return str
-}
-
 fn main() {
 	mut fp := flag.new_flag_parser(os.args)
 	fp.application('Kitty PNG fp')
@@ -93,12 +14,6 @@ fn main() {
 		println(fp.usage())
 		return
 	}
-
-	image_data := os.read_file(image_path) or {''}
-	// kitty.print_png_at_point(image_data)
-
-	ppm := PPM{
-		data: ppm_data
-	}
-	kitty.print_rgb_at_point(ppm.to_string(), ppm_width, ppm_height)
+	image_data := os.read_bytes(image_path) or { []byte{} }
+	kitty.print_png_at_point(image_data)
 }
--- kitty/kitty.v
+++ kitty/kitty.v
@@ -27,24 +27,23 @@ pub enum Display {
 	transmit_nodisplay = 116 // `t`
 }
 
-pub fn print_png_at_point(image_data string) {
+pub fn print_png_at_point(image_data []byte) {
 	print_image(image_data, ',a=T,f=100')
 }
 
-pub fn print_rgb_at_point(image_data string, width u32, height u32) {
+pub fn print_rgb_at_point(image_data []byte, width u32, height u32) {
 	print_image(image_data, ',a=T,f=24,s=$width.str(),v=$height.str()')
 }
 
-pub fn print_rgba_at_point(image_data string, width u32, height u32) {
+pub fn print_rgba_at_point(image_data []byte, width u32, height u32) {
 	print_image(image_data, ',a=T,f=32,s=$width.str(),v=$height.str()')
 }
 
 // https://sw.kovidgoyal.net/kitty/graphics-protocol
-pub fn print_image(image_data string, image_options_str string) {
+pub fn print_image(image_data []byte, image_options_str string) {
 	mut out := os.stdout()
 	image_options := image_options_str.bytes()
-	mut b64_data := base64.encode_str(image_data)
-	// b64_data = image_data
+	mut b64_data := base64.encode(image_data).bytes()
 	mut b64_pos := 0
 	mut chunk_buffer := []byte{len: 0, cap: 4128, init: 0}
 
@@ -70,12 +69,16 @@ pub fn print_image(image_data string, image_options_str string) {
 			}
 			// m == 0 iff the encoded data is the final chunk.
 			if b64_slice.len == 4096 {
-				payload := b64_slice[..4096]
-				serialize_gr_command(payload, 1, []byte{}, mut chunk_buffer)
+				unsafe {
+					payload := b64_slice[..4096]
+					serialize_gr_command(payload, 1, []byte{}, mut chunk_buffer)
+				}
 				out.write(chunk_buffer) or { panic(err) }
 			} else {
-				payload := b64_slice
-				serialize_gr_command(payload, 0, []byte{}, mut chunk_buffer)
+				unsafe {
+					payload := b64_slice
+					serialize_gr_command(payload, 0, []byte{}, mut chunk_buffer)
+				}
 				out.write(chunk_buffer) or { panic(err) }
 				break
 			}
@@ -93,9 +96,10 @@ fn options_map_to_string(options Options) []byte {
 }
 
 // Put an array of bytes in the form Kitty reads.
-fn serialize_gr_command(payload string, m int, image_options []byte, mut buffer []byte) {
+fn serialize_gr_command(payload []byte, m int, image_options []byte, mut buffer []byte) {
 	buffer << '\033_Gm=$m'.bytes()
-	// image_options always starts with a `,` byte.
 	buffer << image_options
-	buffer << ';$payload\033\\'.bytes()
+	buffer << ';'.bytes()
+	buffer << payload
+	buffer << '\033\\'.bytes()
 }
